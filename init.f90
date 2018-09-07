6dd7b8c64ba1b33b085dd2a5516290656bb124dd
Vignesh Murugan <vmur23@monash-student-linux-1604>
2018-09-07 13:06:14 +1000

    working initialise

--- init.f90
+++ init.f90
@@ -1,5 +1,6 @@
 module init
   implicit none
+  real(Kind = 8)::PI = 4.D0*DATAN(1.D0)
   public :: set_initialSquare
 contains
 
@@ -11,11 +12,23 @@ contains
 
     do i = 1,int(size),1
       xGrid(i) = i * dx
-      if(i > 0.25 * size .AND. 1 <= 0.75 * size)THEN
+      if(i > 0.25 * size .AND. i <= 0.75 * size)THEN
         uGrid(i) = 1.
       else
         uGrid(i) = 0.
       end if
+    end do
+  end subroutine
+
+
+  subroutine set_initialSine(xGrid,uGrid,size,dx)
+    real, intent(inout),dimension(:):: xGrid,uGrid
+    real, intent(in):: size,dx
+    integer :: i
+
+    do i = 1,int(size),1
+      xGrid(i) = i * dx
+      uGrid(i) = Sin(2 * PI * i / size)
     end do
   end subroutine
 
--- main.f90
+++ main.f90
@@ -3,18 +3,22 @@ program hydro2
   use init
   implicit none
 
-  real :: v,t,dx,dt
+  real :: v,t,dx,dt,xSize
   integer,parameter :: NX = 100
   real, allocatable, dimension(:) :: x_array,u_array
+
 
   v = 1.
   dx = 0.01
   dt = 0.5 * (dx / v)
+  xSize = 1./dx
 
 
 
+  call set_grid(x_array,u_array,int(xSize))
+  call set_initialSine(x_array,u_array,xSize,dx)
 
-  call set_grid(x_array,u_array,int(1./dx))
-  !call set_initialSquare
+
+  print*, u_array
 
 end program hydro2
